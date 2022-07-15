e65e5d00a5e17890bfdbcc928154eae49c27d044
Luega <martinelliluca90@gmail.com>
2022-07-15 22:31:28 +0200

    added comparison and ending Array and comparison logic

--- src/components/BabboleoWorld copy.vue
+++ src/components/BabboleoWorld copy.vue
@@ -13,9 +13,9 @@
     <div>
       <!-- playgorund -->
       <div class="playground">
-        <!-- run a v-for on the crocodiles array to create cards  -->
+        <!-- run a v-for on the crocodilesArray array to create cards  -->
         <!-- added the click event to change the display mode of the card's img -->
-        <div class="card" v-for="(croc, key) in crocodiles" :key="key" @click="selectCard(croc, key)">
+        <div :id="`a-${key}`" class="card" v-for="(croc, key) in crocodilesArray" :key="key" @click="selectCard(croc, key)">
         <!-- added the id to select the img in the function 'selectCard' -->
           <img :id="`ab-${key}`" :src="require(`@/assets/img/${croc.img}`)" alt="Croc">
         </div>
@@ -32,7 +32,12 @@ export default {
   },
   data() {
     return {
-      crocodiles: [
+      // array for the imgs comparison
+      comparisonArray: [],
+      // array for the ending of the game
+      endingArray: [],
+      // array for the cards
+      crocodilesArray: [
         {
           img: "cartoon-crocodile-vector-21277180.jpg",
           selected: false,
@@ -98,14 +103,35 @@ export default {
   },
   methods: {
     selectCard(croc, key) {
-      // depending on truthfulness of the croc.selected we change it with its opposite and after we change the display mode of the card's img thanks to unique id
+      // depending on truthfulness of the selected 
       if (croc.selected == false) {
-        croc.selected = true;
+        // change selected value of the card 
+        croc.selected = true; 
+        // change the display mode of the card's img
         document.getElementById(`ab-${key}`).style.display = "inline-block";
+        // push the obj in the comparison array
+        this.comparisonArray.push(croc);
+        // after the second card selection
+        if (this.comparisonArray.length == 2) {
+          // if the img of obj are the same
+          if (this.comparisonArray[0].img == this.comparisonArray[1].img) {
+            // push the first obj of comparisonArray in to endingArray
+            this.endingArray.push(this.comparisonArray[0]);
+            // remove the objts from comparisonArray
+            this.comparisonArray.splice(0, 2);
+            alert("hai 0 elementi nell'array");
+          }
+        }
       } else {
         croc.selected = false;
         document.getElementById(`ab-${key}`).style.display = "none";
       }
+      // 1- creo due array vuoti (uno per il confronto e uno per la fine del gioco)
+      // 2- la lunghezza dell'array é minore di 2
+      // 3- vai avanti
+      // 4- la lunghezza dell'array é maggiore o uguale a 2
+      // 5- se img sono uguali allora allert, aggiungo card all'array e poi la card cambia display in none 
+      // 6- se img sono diverse le img display none e l´array viene svuotato.
     },
   }
 }
